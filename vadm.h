29e9af29381cab0811686772fe5047b49a9c0f4c
Constantin Wiemer <constantin.wiemer@gmx.de>
2020-11-01 19:24:23 +0100

    Fixed a comment

--- vadm.h
+++ vadm.h
@@ -7,8 +7,9 @@
 #ifndef VADM_H_INCLUDED
 #define VADM_H_INCLUDED
 
-// base address of Exec library, the only absolute address in the AmigaOS, can't use 4 (the value
-// in the AmigaOS) because it has to be a page boundary as we create a memory mapping there
+// base address of Exec library, the only absolute address in the AmigaOS. We can't use 4
+// (the value in the AmigaOS) because the address has to be a page boundary as we create a
+// memory mapping there, and mmap(2) on Linux doesn't allow a mapping in the first 64KB anyway.
 #define ABS_EXEC_BASE 0x00300000
 
 #endif
